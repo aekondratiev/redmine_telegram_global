acdd831d521c584c983a689d53b53807cb238364
Andry Kondratiev <a.kondratiev@restream.ru>
2017-06-05 21:39:35 +0300

    fix timeout

--- lib/redmine_telegram_global/listener.rb
+++ lib/redmine_telegram_global/listener.rb
@@ -33,8 +33,11 @@ class TelegramListener < Redmine::Hook::Listener
       # client.ssl_config.cert_store.set_default_paths
       # client.ssl_config.ssl_version = "SSLv23"
       # client.post_async url, {:payload => params.to_json}
+      client.connect_timeout = 1
       client.send_timeout = 1
       client.receive_timeout = 1
+      client.keep_alive_timeout = 1
+      client.ssl_config.timeout = 1
       conn = client.post_async(url, params)
       Rails.logger.info("TELEGRAM RETURN CODE: #{conn.pop.status_code}")
     rescue Exception => e
